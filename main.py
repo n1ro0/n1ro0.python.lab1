16a83f108f0644baf6f33a6dd8a66f7623e7b544
Ilya Chaban <n1ro0ilya@gmail.com>
2017-09-12 11:09:34 +0300

    Revert "Adds class work notes"
    This reverts commit 0bc14702c228d252b14cfd52a44888b75741cbe9.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import turtle
 from models.TriangleSerpinskogo import TriangleSerpinskogo as TS
 
+
 t = turtle.Turtle()
-ts = TS(t, (-300, -200), (0, 250), (300, -200)).draw()
+#ts = TS(t, (-300, -200), (0, 250), (300, -200)).draw()
 ts = TS(t, (-300, -200), (0, 250), (300, -200), conter_clockwise=True).draw_reverse()
 input()
-
